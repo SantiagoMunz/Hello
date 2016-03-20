399a6d20ffddda03e7815ed43bbfdf38709de1ba
DreamingD <1576419679@qq.com>
2016-03-20 10:21:25 +0800

    做好了三个界面，并能初步链接
    爱苏小妮

--- xingtingbao/app/src/main/java/com/xingtingbao/xingtingbao/BranchActivity.java
+++ xingtingbao/app/src/main/java/com/xingtingbao/xingtingbao/BranchActivity.java
@@ -1,7 +1,9 @@
 package com.xingtingbao.xingtingbao;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class BranchActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class BranchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_branch);
+        findViewById(R.id.btnbaoxian).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent("android.intent.action.BaoxianMain"));
+            }
+        });
     }
 }
