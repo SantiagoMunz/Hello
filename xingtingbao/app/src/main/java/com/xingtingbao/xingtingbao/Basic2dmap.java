7425fbc384aba481b1f7f17dceb4caf36c4d64f3
SantiagoMunz <467087121@qq.com>
2016-04-02 14:25:29 +0800

    高德sdk的一个activity
    这个activity,是由“开始报险”fragment 打开的。

---
+++ xingtingbao/app/src/main/java/com/xingtingbao/xingtingbao/Basic2dmap.java
@@ -0,0 +1,66 @@
+package com.xingtingbao.xingtingbao;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.MapView;
+
+public class Basic2dmap extends AppCompatActivity {
+    private MapView mapView;
+    private AMap aMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        System.out.println("I arrive here");
+        setContentView(R.layout.activity_amap);
+        mapView = (MapView) findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);// 此方法必须重写
+    }
+    /**
+     * 初始化AMap对象
+     */
+    private void init() {
+        if (aMap == null) {
+            aMap = mapView.getMap();
+
+        }
+
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    /**
+     * 方法必须重写
+     */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+}
